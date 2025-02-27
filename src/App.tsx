ab0443160ad839cee121d66a73facdd7030d6231
Ananth Prathap <22am014@sctce.ac.in>
2025-02-27 09:19:52 +0530

    feat : lol

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import { PageTransition } from "@/components/ui/page-transition";
-import { ScrollToTop } from "@/components/ui/scroll-to-top";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Pricing from "./pages/Pricing";
 import NotFound from "./pages/NotFound";
@@ -13,47 +10,19 @@ import FAQ from "./pages/FAQ";
 
 const queryClient = new QueryClient();
 
-// Create a component to handle the routes with transitions
-const AnimatedRoutes = () => {
-  const location = useLocation();
-  
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={
-          <PageTransition>
-            <Index />
-          </PageTransition>
-        } />
-        <Route path="/pricing" element={
-          <PageTransition>
-            <Pricing />
-          </PageTransition>
-        } />
-        <Route path="/faq" element={
-          <PageTransition>
-            <FAQ />
-          </PageTransition>
-        } />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={
-          <PageTransition>
-            <NotFound />
-          </PageTransition>
-        } />
-      </Routes>
-    </AnimatePresence>
-  );
-};
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <ScrollToTop />
-        <AnimatedRoutes />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/faq" element={<FAQ />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
